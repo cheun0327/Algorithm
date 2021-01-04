be8c6313271540f11aa1b5a503cc3adbc2561117
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2021-01-04 23:16:29 +0900

    Create BOJ_1915_김채은.cpp

---
+++ study/C++2/VCWeek1/BOJ_1915_김채은.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+const int MAX = 1000;
+
+int N, M, ans;
+string arr[MAX+1];
+int dp[MAX+1][MAX+1]; 
+
+int main(){
+
+cin >> N >> M;
+for(int i=0; i<N; ++i)
+  cin >> arr[i];
+
+for(int i=0; i<N; ++i){
+    for(int j=0; j<M; ++j){
+        if(arr[i][j]=='1'){
+            if(i==0 || j==0) dp[i][j] = 1;
+            else            
+                dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            ans = max(ans, dp[i][j]);
+        }
+    }
+}
+
+cout << ans*ans << '\n';
+
+return 0;
+}
