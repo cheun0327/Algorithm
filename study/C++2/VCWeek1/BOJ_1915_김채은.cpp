5c4b33a933096be466df428ad89dadc8f3a70249
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2021-01-05 23:13:09 +0900

    Update BOJ_1915_김채은

--- study/C++2/VCWeek1/BOJ_1915_김채은.cpp
+++ study/C++2/VCWeek1/BOJ_1915_김채은.cpp
@@ -6,15 +6,16 @@ const int MAX = 1000;
 
 int N, M, ans;
 string arr[MAX+1];
-int dp[MAX+1][MAX+1]; 
+int dp[MAX+1][MAX+1];
 
-int main(){
+void Input() {
+  cin >> N >> M;
+  for(int i=0; i<N; ++i)
+    cin >> arr[i];
+}
 
-cin >> N >> M;
-for(int i=0; i<N; ++i)
-  cin >> arr[i];
-
-for(int i=0; i<N; ++i){
+void Solution() {
+  for(int i=0; i<N; ++i){
     for(int j=0; j<M; ++j){
         if(arr[i][j]=='1'){
             if(i==0 || j==0) dp[i][j] = 1;
@@ -23,9 +24,15 @@ for(int i=0; i<N; ++i){
             ans = max(ans, dp[i][j]);
         }
     }
+  }
 }
+  
+int main(){
 
-cout << ans*ans << '\n';
+  Input();
+  
+  Solution();
 
-return 0;
+  cout << ans*ans << '\n';
+  return 0;
 }
