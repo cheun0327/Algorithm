bb3ab8464ff7b32715a44c017a0430bf1da51a65
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2021-01-30 20:40:54 +0900

    Update BOJ_8911_김채은.cpp

--- study/C++2/VCWeek4/BOJ_8911_김채은.cpp
+++ study/C++2/VCWeek4/BOJ_8911_김채은.cpp
@@ -1,56 +1,34 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-typedef struct _Point {
-	int y, x;
-}Point;
 
-int dy[4] = {1,0,-1,0};
-int dx[4] = {0,1,0,-1};
-char command[501];
-
-void read() {
-	scanf("%s",command);
-}
-
-int solve() {
-	Point c = {0,0};
-	int min_x = 0, max_x = 0, min_y = 0, max_y = 0;
-	int d = 0;
-
-	for (int i = 0; command[i] != NULL; i++) {
-		switch (command[i]){
-		case 'L':
-			d = (d + 3) % 4;
-			break;
-		case 'R':
-			d = (d + 1) % 4;
-			break;
-		case 'F':
-			c.y += dy[d], c.x += dx[d];
-			break;
-		case 'B':
-			c.y += dy[(d+2)%4], c.x += dx[(d+2)%4];
-			break;
+int main()
+{
+	int maxX, maxY = 0;
+	int dir = 1;
+	int currX, currY = 0;
+	string input;
+	cin>>input;
+	for(int i=0; i<input.length(); i++)
+	{
+		if (input[i] == 'F') {
+			if (dir == 1) {
+				if (currmaxX++;
+				currX++;
+			} else {
+				maxY++;
+				currY++;
+			}
 		}
-
-		if (c.x > max_x) max_x = c.x;
-		if (c.x < min_x) min_x = c.x;
-		if (c.y > max_y) max_y = c.y;
-		if (c.y < min_y) min_y = c.y;
-	}
-	
-	return (max_x - min_x)*(max_y - min_y);
-}
-
-int main() {
-	int T;
-	scanf("%d",&T);
-
-	for (int t_case = 1; t_case <= T; t_case++) {
-		read();
-		int answer = solve();
-		printf("%d\n",answer);
+		else if(input[i] == 'B')
+		{
+			if (dir == 1)
+				currX++;
+			else 
+				currY++;
+		}
+		else if(input[i] == 'L')
 	}
 	return 0;
 }
